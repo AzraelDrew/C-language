308e9609a045462e63d996a23ad26eadac99ac66
Azrael <740161192@qq.com>
2021-01-01 00:25:22 +0800

    2021.1.1

--- MOOC/Week Three/number.c
+++ MOOC/Week Three/number.c
@@ -2,25 +2,22 @@
 int main()
 {
     int x;
-    int n = 1;
+    int n = 0;
     printf("请输入一个整数:");
     scanf("%d", &x);
-    // if (x > 999)
-    // {
-    //     n = 4;
-    // }
-    // else if (x > 99)
-    // {
-    //     n = 3;
-    // }
-    // else if (x > 9)
-    // {
-    //     n = 2;
-    // }
-    // else
-    // {
-    //     n = 1;
-    // }
+    n++;
+    if (x < 0)
+    {
+        x = -x;
+    }
+    x /= 10;
+    while (x > 0)
+    {
+        n++;
+        x /= 10;
+        printf("x=%d,n=%d\n", x, n);
+    }
+
     printf("你输入的数为%d位数\n", n);
     return 0;
 }
