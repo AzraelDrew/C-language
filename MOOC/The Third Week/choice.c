5043e26bfcca659d18abf1e85de1a09ffd43d642
Azrael <740161192@qq.com>
2021-01-04 15:46:27 +0800

    rename folder

---
+++ MOOC/The Third Week/choice.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int i ;
+    int arr[] = {1,2,3,4,5};
+    for (i=01;i<=5;i++){
+    printf("i=%d\n",i);
+    }
+    printf("最后i=%d\n",i);
+}
+
+// 如果有固定次数,用for
+// 如果必须执行一次,用do while
+//其他情况用while
