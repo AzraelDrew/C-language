6aa394b9bb539647247f70db3d340f3ecc3ecee7
Azrael <yznofsy@gmail.com>
2021-09-15 19:42:13 +0800

    update

--- chapter01/1.cpp
+++ chapter01/1.cpp
@@ -18,34 +18,34 @@ int x=-1,y=n-1;
 int dir[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
 int k=0;
 while(count<m*n){
-  //  k%=4;
-  // if((x+dir[k][0]>m-1)||(y+dir[k][1]>n-1)||(x+dir[k][0]<0)||(y+dir[k][1]<0)||array[x+dir[k][0]][y+dir[k][1]]!=0){
-  //   k++;
-  //   continue;
+   k%=4;
+  if((x+dir[k][0]>m-1)||(y+dir[k][1]>n-1)||(x+dir[k][0]<0)||(y+dir[k][1]<0)||array[x+dir[k][0]][y+dir[k][1]]!=0){
+    k++;
+    continue;
+  }
+  x+=dir[k][0];
+  y+=dir[k][1];
+  array[x][y]=count++;
+  // while(x<m-1 && array[x+1][y]==0){
+  //   x++;
+  //   array[x][y] = count;
+  //   count++;
   // }
-  // x+=dir[k][0];
-  // y+=dir[k][1];
-  // array[x][y]=count++;
-  while(x<m-1 && array[x+1][y]==0){
-    x++;
-    array[x][y] = count;
-    count++;
-  }
-  while(y>=-1 && array[x][y-1]==0){
-    y--;
-    array[x][y] = count;
-    count++;
-  }
-  while(x>0 && array[x-1][y]==0){
-    --x;
-    array[x][y] = count;
-    count++;
-  }
-  while(y<n-1 && array[x][y+1]==0){
-    ++y;
-    array[x][y] = count;
-    count++;
-  }
+  // while(y>=-1 && array[x][y-1]==0){
+  //   y--;
+  //   array[x][y] = count;
+  //   count++;
+  // }
+  // while(x>0 && array[x-1][y]==0){
+  //   --x;
+  //   array[x][y] = count;
+  //   count++;
+  // }
+  // while(y<n-1 && array[x][y+1]==0){
+  //   ++y;
+  //   array[x][y] = count;
+  //   count++;
+  // }
 }
 
   for(int i=0;i<m;++i){
