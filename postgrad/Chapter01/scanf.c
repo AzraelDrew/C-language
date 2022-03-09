10c5504994155688adc1185d3409510936c9bb29
Azrael <yznofsy@gmail.com>
2022-03-09 21:40:37 +0800

    update

---
+++ postgrad/Chapter01/scanf.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+#define PI 3   //定义符号常量
+
+int main()
+{
+	int a=1;//a就是一个变量
+    a = 5;
+    //PI = 10;   //不能给常量赋值  
+    printf("%d\n",PI);
+    return 0;
+}
---
+++ postgrad/Chapter01/常量与变量.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+#define PI 3.14   //定义符号常量
+
+int main()
+{
+	int a=1;//a就是一个变量
+    a = 5;
+    //PI = 10;   //不能给常量赋值  
+    return 0;
+    printf("%d\n",PI);
+}
