92acd65c19f8f43da27a0c12d2d7ff80d031cbf8
Azrael <yznofsy@gmail.com>
2022-03-27 22:52:51 +0800

    update

--- test.c
+++ test.c
@@ -1,6 +1,26 @@
-#include "stdio.h"
+#include <string.h>
+#include <stdio.h>
+
 int main()
 {
-	printf("Hello World!\n");
-	return 0;
+char c[100],d[100];
+gets(c);
+int i,j;
+for(i=strlen(c)-1,j=0;i>=0;i--,j++)
+{
+	d[j] = c[i];
+}
+d[j]  = '\0';
+int result = strcmp(c, d);
+if (result < 0)
+{
+printf("%d\n",-1);
+}
+else if (result > 0)
+{
+printf("%d\n", 1);
+}
+else {
+printf("%d\n", 0);
+}
 }
